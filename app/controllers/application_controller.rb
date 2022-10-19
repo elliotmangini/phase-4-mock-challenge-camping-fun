09aefed0c69cef902637d954c0901da521c9cf77
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-19 10:59:17 -0600

    commit tree 2c0d7612041d1d7178dfee2d216967915e06361e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  private
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
+
 end
